a2f93a72d370b7a9a499a5e9a940ed74c432988d
Bikash Das <bikashd003@gmail.com>
2024-01-27 02:52:24 +0530

    add all quiz api and analysis quiz api

--- Controllers/ViewAnalytics.controller.js
+++ Controllers/ViewAnalytics.controller.js
@@ -16,7 +16,54 @@ const viewQuiz = async (req, res) => {
         errorHandler(res, error)
     }
 }
-const viewQuizAnalysis=async(req,res)=>{
+const viewQuizAnalysis = async (req, res) => {
+    const { quizId } = req.body;
+    try {
+        const quiz = await quizSchema.findById(quizId)
+        const poll = await pollSchema.findById(quizId)
+        if (!quiz && !poll) {
+            return res.status(400).json({ message: "No quiz found" })
+        }
+        if (quiz) {
+            res.status(200).json(quiz)
+        }
+        else if (poll) {
+            res.status(200).json(poll)
+        }
+        else {
+            res.status(400).json({ message: "No quiz found" })
+        }
+    }
+    catch (error) {
+        errorHandler(res, error)
+    }
 
 }
-export {viewQuiz,viewQuizAnalysis};
+const allQuiz = async (req, res) => {
+    const adminId = req.admin._id;
+    try {
+        const quiz = await adminSchema.findById(adminId).populate("createdQuiz");
+        const poll = await adminSchema.findById(adminId).populate("createdPoll");
+
+        const quizzes = quiz.createdQuiz;
+        const polls = poll.createdPoll;
+
+        const numberOfQAndA = quizzes.map(q => q.questions.length).reduce((total, length) => total + length, 0);
+        const numberOfPolls = polls.map(p => p.polls.length).reduce((total, length) => total + length, 0);
+
+        const numberOfQuestions = numberOfQAndA + numberOfPolls;
+        const totalImpressions = quizzes.map(q => q.impression).reduce((total, impression) => total + impression, 0) +
+            polls.map(p => p.impression).reduce((total, impression) => total + impression, 0);
+
+        const allDetails = [...quizzes, ...polls];
+        const trendingQuiz = allDetails.sort((a, b) => b.impression - a.impression);
+
+        res.status(200).json({ allDetails, numberOfQuestions, totalImpressions, trendingQuiz });
+    } catch (error) {
+        errorHandler(res, error);
+    }
+};
+
+
+
+export { viewQuiz, viewQuizAnalysis, allQuiz };
