e41b6f932422c4190a09479e08d588b44b64245f
Bikash Das <bikashd003@gmail.com>
2024-01-28 03:08:37 +0530

    add update quiz controller file,not code

---
+++ Controllers/UpdateQuiz.controller.js
@@ -0,0 +1,6 @@
+import pollSchema from "../Models/Poll.models.js"
+import quizSchema from "../Models/Quiz.models.js"
+const updateQuiz=async(req,res)=>{
+
+}
+export default updateQuiz;
--- Routes/ViewAnalytics.routes.js
+++ Routes/ViewAnalytics.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
-import {viewQuiz} from "../Controllers/ViewAnalytics.controller.js";
+import { viewQuiz, viewQuizAnalysis, allQuiz } from "../Controllers/ViewAnalytics.controller.js";
 import isLoggedIn from "../Middleware/Auth.middleware.js"
 const viewQuizRouter = Router();
 
-viewQuizRouter.get("/view-quiz",isLoggedIn, viewQuiz);
+viewQuizRouter.get("/view-quiz", isLoggedIn, viewQuiz);
+viewQuizRouter.post("/view-quiz-analysis", isLoggedIn, viewQuizAnalysis);
+viewQuizRouter.get("/all-quiz", isLoggedIn, allQuiz);
 export default viewQuizRouter;
